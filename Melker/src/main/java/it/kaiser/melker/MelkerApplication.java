3995b9e687376680218aa7edf2eef3ccd80c6925
Alfonso Spisto <alfonso.spisto.external@atos.net>
2023-04-19 16:49:26 +0200

    aggiunta MELKERONE come microservizio di auth

---
+++ Melker/src/main/java/it/kaiser/melker/MelkerApplication.java
@@ -0,0 +1,13 @@
+package it.kaiser.melker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelkerApplication.class, args);
+	}
+
+}
---
+++ Melker/src/test/java/it/kaiser/melker/MelkerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.kaiser.melker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
